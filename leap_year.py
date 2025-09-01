0b5f992f1c1045ec1454cc17a13375848307cbad
riveirasanti <riveirasanti@gmail.com>
2025-09-01 19:04:23 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,5 @@
 def leap_year():
-   anio=int(input("Ingrese un anio: "))
+   anio=int(input("Ingrese un año: "))
    if(anio % 4 == 0) and ((anio % 100 !=0) or (anio % 400 == 0)):
        print(f"El año {anio} es bisiesto") 
    else:

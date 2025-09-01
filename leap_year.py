a39b1179f5f357e70e0b372f40185327f4251af6
riveirasanti <riveirasanti@gmail.com>
2025-09-01 19:12:20 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,6 @@
 def leap_year():
    anio=int(input("Ingrese un año: "))
-   if(anio % 4 == 0) and ((anio % 100 !=0) or (anio % 400 == 0)):
+   if (anio % 4 == 0) and ((anio % 100 != 0) or (anio % 400 == 0)):
        print(f"El año {anio} es bisiesto") 
    else:
        print(f"El año {anio} no es bisiesto")    
--- line.py
+++ line.py
@@ -7,23 +7,26 @@ def line():
     x2=float(input("Ingrese el coeficiente X2: "))
 
 
-    y1= a*x1+b
-    y2= a*x2+b
+    y1= a * x1+ b
+    y2= a * x2+ b
 
-    distancia=math.sqrt((x2-x1)**2+ (y2-y1)**2)
+    distancia=math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
     print(f"El coeficiente A de su ecuación de la recta es: {a}")
     print(f"El coeficiente B de su ecuación de la recta es: {b}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
-
+    print("")
 
     print("\nPara la siguiente ecuación:")
     print(f"\tY = {a}X + {b}")
+    print("")
 
     print("\nDados los siguientes puntos:")
     print(f"\tP1 ({x1}, {y1})")
     print(f"\tP2 ({x2}, {y2})")
+    print("")
+    
 
     print(f"\nLa distancia entre ellos es: {distancia}")
 
